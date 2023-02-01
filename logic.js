714f54e06b521259ccf46e9d378f795c86e93111
samvrny <samvrny@gmail.com>
2023-01-31 23:17:11 -0600

    got the users spaceship moving

--- logic.js
+++ logic.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.getElementById('result');
 
     let width = 15;
+    let height = 15;
     let currentSpaceshipIndex = 202;
     let currentEnemyIndex = 0;
     let enemiesKilled = [];
@@ -24,4 +25,29 @@ document.addEventListener('DOMContentLoaded', () => {
     //draw user spaceship 
     blocks[currentSpaceshipIndex].classList.add('spacecraft')
 
+    //make the spaceship left to right
+    function maneuverSpaceship(event) {
+        blocks[currentSpaceshipIndex].classList.remove('spacecraft')
+        switch(event.keyCode) {
+            //left and right
+            case 37:
+                if(currentSpaceshipIndex % width !== 0) currentSpaceshipIndex -=1
+                break
+            case 39:
+                if(currentSpaceshipIndex % width < width - 1) currentSpaceshipIndex +=1
+                break
+
+            //up and down TODO: Add up and down functionality
+            case 38:
+                if(currentSpaceshipIndex >= height + 1) currentSpaceshipIndex -=15
+                break
+            case 40:
+                if(currentSpaceshipIndex <= height * 15 - 16) currentSpaceshipIndex +=15
+        }
+
+        blocks[currentSpaceshipIndex].classList.add('spacecraft')
+    }
+
+    //listen for keys being pressed to move the spaceship
+    document.addEventListener('keydown', maneuverSpaceship)
 })
